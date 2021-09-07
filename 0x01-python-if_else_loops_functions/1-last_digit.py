a2ee4185aa6238a413352f1d0dc04e920db53658
kjnat <my24playstation@gmail.com>
2021-09-07 10:01:38 -0700

    1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#1/bin/usr/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+if last_digit > 5:
+    print("Last digit of" + number + "is" + last_digit + "and is greater than 5")
+if last_digit == 0:
+    print("Last digit of" + number + "is" + last_digit + "and is 0")
+if last_digit < 6 && last_digit != 0:
+    print("Last digit of" + number + "is" + last_digit + "and is less than 6 and not 0")
