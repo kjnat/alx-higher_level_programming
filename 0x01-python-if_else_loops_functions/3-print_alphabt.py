ffe97cff20f2177cc263ab4f3c518e8561a70d2b
kjnat <46638938+kjnat@users.noreply.github.com>
2021-09-07 17:46:36 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-   if character != 101 and character != 113: 
-      print("{:c}".format(character), end='')
+for ch in range(97, 123):
+        if ch != 101 and ch != 113:
+                print("{:c}".format(ch), end='')
