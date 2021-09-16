45465b3270deb2bf305cf4c6a5d3d8e2567dcfa5
kjnat <46638938+kjnat@users.noreply.github.com>
2021-09-16 15:27:05 +0000

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
