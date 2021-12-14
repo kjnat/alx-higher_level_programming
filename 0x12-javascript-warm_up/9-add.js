7a275245b06ed4b24a6ade929ba4c35c23db39ed
kjnat <46638938+kjnat@users.noreply.github.com>
2021-12-14 20:30:11 +0000

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-const firstArg = parseInt(process.argv[2]);
-const secondArg = parseInt(process.argv[3]);
-
 function add (a, b) {
-  if (a && b) {
-    return (a + b);
-  }
-  return NaN;
+  return a + b;
 }
-console.log(add(firstArg, secondArg));
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
