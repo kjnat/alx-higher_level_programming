0809700c34efe5023d5af0a846483e78d8e26c63
kjnat <46638938+kjnat@users.noreply.github.com>
2021-12-14 20:28:15 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myStrings.length; i++) {
-  console.log(myStrings[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
